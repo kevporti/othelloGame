16afdb65b0ece7fb81d5e08d7c6853090efd2bc2
Juan Cruz D'Amor <damorjuancruz@gmail.com>
2022-10-20 11:37:19 -0300

    Validacion de movimientos cuando no se saltea el turno

--- index.py
+++ index.py
@@ -40,26 +40,97 @@ def leerArchivo():
 
   return jugadores, jugadas
 
+def parsearJugada(jugada):
+  color, posicion = jugada
+  columnaJugada = ord(posicion[0]) - ord('A')
+  filaJugada = int(posicion[1]) - 1
+
+  return color, filaJugada, columnaJugada
+
+#Checkear si la posicion de la jugada no esta ocupada
+def reglaUno(tablero, jugada):
+  _, filaJugada, columnaJugada = parsearJugada(jugada)
+  if tablero[filaJugada][columnaJugada] != " ":
+    raise Exception(f'La posicion {jugada[1]} ya esta ocupada')
+
 # Determinar si la posicion de la jugada coincide con tener al menos una ficha del oponente alrededor de la nueva ficha,
 # sino, es una jugada invalida. Si la jugada es invalida, se produce una excepcion.
-def reglaUno(tablero, jugada):
+# Tambien determinar si hay una ficha aliada que encierre a la enemiga para poder cambiar el color y validar el movimiento.
+def reglaDos(tablero, jugada):
+  color, filaJugada, columnaJugada = parsearJugada(jugada)
+
+  if color == 'B':
+    colorOponente = 'N'
+  else:
+    colorOponente = 'B'
+  
+  hayOponenteAlrededor = False
+  hayFichasEncerradas = False
+
+  # Recorrer todas las posiciones del tablero que estan alrededor de la jugada realizada
+  for i in range(-1, 2):
+    for j in range(-1, 2):
+      # Quitando el caso de chequear la posicion de la jugada, vemos las posiciones de alrededor.
+      if i != 0 or j != 0:
+        filaCheckear = filaJugada + i
+        columnaCheckear = columnaJugada + j
+
+        # Checkeando que las posiciones a comparar no se salgan de los parametros del tablero
+        seSaleDelTablero = filaCheckear >= 8 or filaCheckear < 0 or columnaCheckear >= 8 or columnaCheckear < 0
+
+        fichasEnemigas = 0
+        # Recorriendo filas, columnas y diagonales mientras haya una ficha enemiga, para determinar si al final
+        # de todas las fichas hay una aliada que encierre las enemigas y poder validar la jugada.
+        while not seSaleDelTablero and tablero[filaCheckear][columnaCheckear] == colorOponente:
+          fichasEnemigas += 1
+
+          # Avanzar en cada direccion
+          filaCheckear += i
+          columnaCheckear += j
+
+          seSaleDelTablero = filaCheckear >= 8 or filaCheckear < 0 or columnaCheckear >= 8 or columnaCheckear < 0
+        
+        # Hay al menos una ficha enemiga adyacente en esa direccion?
+        if fichasEnemigas != 0:
+          hayOponenteAlrededor = True
+        
+        # Hay una ficha aliada que encierra a las enemigas en esa direccion?
+        if not seSaleDelTablero and tablero[filaCheckear][columnaCheckear] == color:
+          hayFichasEncerradas = True
+
+  # Tirar excepciones si la jugada es invalida
+  if not hayOponenteAlrededor:
+    raise Exception(f'La posicion {jugada[1]} no tiene ningua ficha del oponente alrededor.')
+
+  if not hayFichasEncerradas:
+    raise Exception(f'La posicion {jugada[1]} no encierra fichas del oponente.')
+
+# Checkear que si o si era necesario saltar el turno
+def reglaTres(tablero, jugada):
   pass
 
-# Determinar si la posicion de la jugada coincide con la reglaUno y en ese espacio (vertical, horizontal u oblicuo)
-# se encuentra una ficha aliada para encerrar esas fichas enemigas y convertirse en una jugada valida. Si la jugada
-# es invalida, se produce una excepcion.
-def reglaDos(tablero, jugada):
-  pass
 
 # Dada una jugada valida, aplica la misma en el tablero
 def aplicarJugada(tablero, jugada):
-  pass
+  color, filaJugada, columnaJugada = parsearJugada(jugada)
+
+  tablero[filaJugada][columnaJugada] = color
 
 # Checkea cada jugada (llamando a las respectivas funciones de cada regla) y, si es valida, aplica el resultado de la misma al tablero
 def simularJuego(tablero, jugadas):
   for jugada in jugadas:
-    reglaUno(tablero, jugada)
-    reglaDos(tablero, jugada)
+    color, filaJugada, columnaJugada = parsearJugada(jugada)
+
+    if color != ' ':
+      reglaUno(tablero, jugada)
+      reglaDos(tablero, jugada)
+      aplicarJugada(tablero, jugada)
+    else:
+      reglaTres(tablero, jugada)
+    
+    print(jugada)
+    imprimirTablero(tablero)
+    print()
 
 # Recibe el tablero y devuelve el nombre del jugador, basado en la cantidad de fichas de cada color
 def determinarGanador(tablero):
@@ -67,19 +138,37 @@ def determinarGanador(tablero):
 
 # Muestra el tablero con el formato adecuado
 def imprimirTablero(tablero):
-  pass
+  print('  A B C D E F G H')
+  for i in range(8):
+    nroFila = i + 1
+    contenidoFila = ' '.join(tablero[i])
+    print(f'{nroFila} {contenidoFila} {nroFila}')
+  print('  A B C D E F G H')
+
+def construirTableroInicial():
+  tablero = []
+  for i in range(8):
+    fila = []
+    for j in range(8):
+      if (i == 3 and j == 3) or (i == 4 and j == 4):
+        fila.append("B")
+      elif (i == 3 and j == 4) or (i == 4 and j == 3):
+        fila.append("N")
+      else:
+        fila.append(' ')
+    tablero.append(fila)
+  return tablero
 
 # Punto de entrada del programa
 def main():
-  tablero = [[0 for _ in range(8)] for _ in range(8)]
-
+  tablero = construirTableroInicial()
   jugadores, jugadas = leerArchivo()
 
   try:
     simularJuego(tablero, jugadas)
-  except:
+  except Exception as e:
     imprimirTablero(tablero)
-    # mostrar jugada invalida
+    print(str(e))
   else:
     imprimirTablero(tablero)
     determinarGanador(tablero)
